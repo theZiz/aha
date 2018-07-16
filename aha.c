ef6b6af1d365807741ab493d120ba94659d6e0cf
suve <veg@svgames.pl>
2018-07-16 19:16:03 +0200

    Use the ISO codepage number in <meta> HTTP-equiv tag

--- aha.c
+++ aha.c
@@ -289,7 +289,7 @@ int main(int argc,char* args[])
 		printf("<?xml version=\"1.0\" encoding=\"%s\" ?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n", encoding);
 		printf("<!-- This file was created with the aha Ansi HTML Adapter. https://github.com/theZiz/aha -->\n");
 		printf("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
-		printf("<head>\n<meta http-equiv=\"Content-Type\" content=\"application/xml+xhtml; charset=UTF-8\" />\n");
+		printf("<head>\n<meta http-equiv=\"Content-Type\" content=\"application/xml+xhtml; charset=%s\" />\n", encoding);
 		
 		printf("<title>");
 		printHtml(title ? title : filename ? filename : "stdin");
